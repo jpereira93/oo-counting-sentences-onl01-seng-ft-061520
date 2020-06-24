31edf67f9279223b7615697ff0eb5cfdc5cd21c7
Jessica Pereira <jcpereira070@gmail.com>
2020-06-24 18:52:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
+  # self.end_with?(".")
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
 
+    self.split(/\.|\?|\!/).delete_if do |s|
+
+      s.length <2
+
+    end.count
   end
+
+
+
+
 end
