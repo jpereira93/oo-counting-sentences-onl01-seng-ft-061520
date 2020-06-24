abf88335c21c52786c2787db8f874703b63d286d
Jessica Pereira <jcpereira070@gmail.com>
2020-06-24 18:45:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,8 +18,8 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if do |s| s.length <2
-
+    self.split(/\.|\?|\!/).delete_if
+    do |s| s.length <2
     end.count 
   end
 
